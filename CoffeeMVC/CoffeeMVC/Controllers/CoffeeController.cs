f732a9ca2b794e80e62adba7070568d011282e0b
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-03-12 08:22:00 +0000

    base files added

---
+++ CoffeeMVC/CoffeeMVC/Controllers/CoffeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeMVC.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        // GET: Coffee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // About
+        public ActionResult About()
+        {
+            return View();
+        }
+        // Coffee
+        public ActionResult Coffee()
+        {
+            return View();
+        }
+    }
+}
